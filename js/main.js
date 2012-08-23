f0c7a32c97fe213e4d48e5e64773ab580cdc3257
Marco Büttner <support@scidev.eu>
2012-08-23 09:29:53 +0200

    main.js
    Add here your javascript code

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function appInit()
+{
+	check(); // please don't remove
+
+	// Portrait-Mode
+	if(g_currentDirection == g_PORTRAIT) {
+			// here your code
+	}
+	// Landscape-Mode
+	else if(g_currentDirection == g_LANDSCAPE) {
+			// here your code
+	}
+}
+
