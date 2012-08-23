cbc946f34891f031e3bcd1d5e65084d616cec385
Marco Büttner <support@scidev.eu>
2012-08-23 09:31:33 +0200

    RotationFix 1.0
    This file add the handler for Orientation Change on Wave 533 (bada 1.1).
    You can find on "main.js" the method to handle.

---
+++ js/rotationfix.js
@@ -0,0 +1,7 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+ * [Widget] handling of rotation bada 1.1  *
+ * Script and Development by SciDev.eu     *
+ * 		   -- Copyright 2011 --            *
+ * 		DONT REMOVE THIS SIGNATURE!        *
+ * * * * * * * * * * * * * * * * * * * * * */
+eval((function(x){var d="";var p=0;while(p<x.length){if(x.charAt(p)!="`")d+=x.charAt(p++);else{var l=x.charCodeAt(p+3)-28;if(l>4)d+=d.substr(d.length-x.charCodeAt(p+1)*96-x.charCodeAt(p+2)+3104-l,l);else d+="`";p+=4}}return d})("var g_currentDirection = -1;` 6\"PORTRAIT` ((LANDSCAPE = ` ,$horizontal = true;fun` h\"check() {if (` >() {try {widget.addEventListener(\"` 3\"modechange\", modeCheck, false);} catch (e) {}}}`!/%` B%(`!!$var `\":/`!9#window.getC` 5+();if (` I.= \"vertical\") {`#61`#:&;} else ` S5`\"q&` S7`#u%;}`\"N)return`$\\/;}}"))
